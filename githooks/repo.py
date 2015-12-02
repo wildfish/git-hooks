d17476409a6c19b81d5b2bfa9fba1ce5d44a4a81
Dan Bate <dan@wildfish.com>
2015-12-02 23:42:05 +0000

    added repo diff inspection functions

---
+++ githooks/repo.py
@@ -0,0 +1,67 @@
+import git
+import sys
+import os
+
+_repo = None
+
+
+def get():
+    """
+    Gets the repo object for the current git repo. This will track back through parent parent directories find the
+    repo root.
+
+    :return: The git repo object (object details can be found here http://gitpython.readthedocs.org/en/stable/tutorial.html#meet-the-repo-type)
+    """
+    global _repo
+    if not _repo:
+        _repo = git.Repo(os.path.dirname(sys.argv[0]), search_parent_directories=True)
+    return _repo
+
+
+def repo_root():
+    """
+    Gets the root directory of the git repo
+
+    :return: The root directory of the git repo
+    """
+    return os.path.dirname(get().git_dir)
+
+
+def untracked_files():
+    """
+    Gets a list of the untracked files in the current git repo
+
+    :return: A list of absolute paths to untracked files in the repo.
+    """
+    repo_root_dir = repo_root()
+    return [os.path.join(repo_root_dir, p) for p in get().untracked_files]
+
+
+def modified_files():
+    """
+    Gets a list of modified files in the repo.
+
+    :return: A list of absolute paths to all changed files in the repo
+    """
+    repo_root_dir = repo_root()
+    return [os.path.join(repo_root_dir, d.a_path) for d in get().index.diff('HEAD') if not (d.new_file or d.deleted_file)]
+
+
+def added_files():
+    """
+    Gets a list of added files in the repo.
+
+    :return: A list of absolute paths to all added files in the repo
+    """
+    repo_root_dir = repo_root()
+    return [os.path.join(repo_root_dir, d.a_path) for d in get().index.diff('HEAD') if d.new_file]
+
+
+def deleted_files():
+    """
+    Gets a list of deleted files in the repo.
+
+    :return: A list of absolute paths to all deleted files in the repo
+    """
+    repo_root_dir = repo_root()
+    return [os.path.join(repo_root_dir, d.b_path) for d in get().index.diff('HEAD') if d.deleted_file]
---
+++ tests/test_repo.py
@@ -0,0 +1,138 @@
+import string
+import sys
+from unittest import TestCase
+
+import os
+from hypothesis import given
+from hypothesis.strategies import text, lists
+from mock import patch, Mock
+
+from githooks import repo
+
+
+class FakeDiffObject(object):
+    def __init__(self, a_path, b_path, new, deleted):
+        self.a_path = a_path
+        self.b_path = b_path
+        self.new_file = new
+        self.deleted_file = deleted
+
+
+class RepoGet(TestCase):
+    @patch('githooks.repo.git')
+    def test_result_is_repo_created_from_the_parent_of_script_directory(self, git_mock):
+        git_mock.Repo = Mock(return_value='git repo')
+
+        repo_obj = repo.get()
+
+        self.assertEqual('git repo', repo_obj)
+        git_mock.Repo.assert_called_once_with(
+            os.path.dirname(sys.argv[0]),
+            search_parent_directories=True,
+        )
+
+
+class RepoRepoRoot(TestCase):
+    @patch('githooks.repo.get')
+    def test_result_is_the_parent_directory_of_the_git_diretory(self, get_mock):
+        git_dir = os.path.dirname(__file__)
+        result = Mock()
+        result.git_dir = git_dir
+
+        get_mock.return_value = result
+
+        self.assertEqual(os.path.dirname(git_dir), repo.repo_root())
+
+
+class RepoUntrackedFiles(TestCase):
+    @patch('githooks.repo.get')
+    def test_result_is_untracked_files_from_the_repo_object(self, get_mock):
+        git_dir = os.path.dirname(__file__)
+
+        result = Mock()
+        result.untracked_files = ['untracked files']
+        result.git_dir = git_dir
+
+        get_mock.return_value = result
+
+        files = repo.untracked_files()
+
+        self.assertListEqual([os.path.join(repo.repo_root(), 'untracked files')], files)
+
+
+class RepoModifiedFiles(TestCase):
+    @given(
+        lists(text(min_size=1, max_size=10, alphabet=string.ascii_letters), max_size=10),
+        lists(text(min_size=1, max_size=10, alphabet=string.ascii_letters), max_size=10),
+        lists(text(min_size=1, max_size=10, alphabet=string.ascii_letters), max_size=10),
+    )
+    def test_result_is_the_absolute_paths_to_all_changed_but_not_new_or_deleted_files(self, mod, new, deleted):
+        mod_diffs = [FakeDiffObject(f, f, False, False) for f in mod]
+        new_diffs = [FakeDiffObject(f, None, True, False) for f in new]
+        deleted_diffs = [FakeDiffObject(None, f, False, True) for f in deleted]
+
+        with patch('githooks.repo.get') as get_mock:
+            git_dir = os.path.dirname(__file__)
+
+            result = Mock()
+            result.index.diff = Mock(return_value=mod_diffs + new_diffs + deleted_diffs)
+            result.git_dir = git_dir
+
+            get_mock.return_value = result
+
+            files = repo.modified_files()
+
+            self.assertEqual([os.path.join(repo.repo_root(), f) for f in mod], files)
+            result.index.diff.assert_called_once_with('HEAD')
+
+
+class RepoAddedFiles(TestCase):
+    @given(
+        lists(text(min_size=1, max_size=10, alphabet=string.ascii_letters), max_size=10),
+        lists(text(min_size=1, max_size=10, alphabet=string.ascii_letters), max_size=10),
+        lists(text(min_size=1, max_size=10, alphabet=string.ascii_letters), max_size=10),
+    )
+    def test_result_is_the_absolute_paths_to_all_new_but_not_modified_or_deleted_files(self, mod, new, deleted):
+        mod_diffs = [FakeDiffObject(f, f, False, False) for f in mod]
+        new_diffs = [FakeDiffObject(f, None, True, False) for f in new]
+        deleted_diffs = [FakeDiffObject(None, f, False, True) for f in deleted]
+
+        with patch('githooks.repo.get') as get_mock:
+            git_dir = os.path.dirname(__file__)
+
+            result = Mock()
+            result.index.diff = Mock(return_value=mod_diffs + new_diffs + deleted_diffs)
+            result.git_dir = git_dir
+
+            get_mock.return_value = result
+
+            files = repo.added_files()
+
+            self.assertEqual([os.path.join(repo.repo_root(), f) for f in new], files)
+            result.index.diff.assert_called_once_with('HEAD')
+
+
+class RepoDeletedFiles(TestCase):
+    @given(
+        lists(text(min_size=1, max_size=10, alphabet=string.ascii_letters), max_size=10),
+        lists(text(min_size=1, max_size=10, alphabet=string.ascii_letters), max_size=10),
+        lists(text(min_size=1, max_size=10, alphabet=string.ascii_letters), max_size=10),
+    )
+    def test_result_is_the_absolute_paths_to_all_deleted_but_not_new_or_modified_files(self, mod, new, deleted):
+        mod_diffs = [FakeDiffObject(f, f, False, False) for f in mod]
+        new_diffs = [FakeDiffObject(f, None, True, False) for f in new]
+        deleted_diffs = [FakeDiffObject(None, f, False, True) for f in deleted]
+
+        with patch('githooks.repo.get') as get_mock:
+            git_dir = os.path.dirname(__file__)
+
+            result = Mock()
+            result.index.diff = Mock(return_value=mod_diffs + new_diffs + deleted_diffs)
+            result.git_dir = git_dir
+
+            get_mock.return_value = result
+
+            files = repo.deleted_files()
+
+            self.assertEqual([os.path.join(repo.repo_root(), f) for f in deleted], files)
+            result.index.diff.assert_called_once_with('HEAD')
