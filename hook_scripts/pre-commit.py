66f2a8d0fe59e7fd76b0b5d2599be9b01611dfdf
Dan Bate <dan@wildfish.com>
2015-12-03 23:45:45 +0000

    changed the pre-commit script to use the runner

--- hook_scripts/pre-commit.py
+++ hook_scripts/pre-commit.py
@@ -1,28 +1,8 @@
 #!/usr/bin/env python
 import sys
 
-import subprocess
-
-from githooks import hooks, repo
-
-
-def main():
-    added_files = repo.added_files()
-    modified_files = repo.modified_files()
-    deleted_files = repo.deleted_files()
-
-    result = 0
-
-    for path in hooks.PreCommitHookFinder():
-        result += subprocess.call(
-            [path] +
-            ['--added-files'] + added_files +
-            ['--modified-files'] + modified_files +
-            ['--deleted-files'] + deleted_files
-        )
-
-    return result
+from githooks import runners
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    sys.exit(runners.PreCommitHookRunner().run())
