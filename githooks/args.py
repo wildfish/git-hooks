634015ac2ff7dc74568131ecbb034cf10b6aeb63
Dan Bate <dan@wildfish.com>
2016-08-24 17:34:02 +0100

    added defaults for files args

--- githooks/args.py
+++ githooks/args.py
@@ -4,8 +4,8 @@ import argparse
 def pre_commit():
     parser = argparse.ArgumentParser()
     parser.add_argument('files', nargs='*')
-    parser.add_argument('--modified-files', nargs='*', dest='modified')
-    parser.add_argument('--added-files', nargs='*', dest='added')
-    parser.add_argument('--deleted-files', nargs='*', dest='deleted')
+    parser.add_argument('--modified-files', nargs='*', dest='modified', default=[])
+    parser.add_argument('--added-files', nargs='*', dest='added', default=[])
+    parser.add_argument('--deleted-files', nargs='*', dest='deleted', default=[])
 
     return parser.parse_args()
