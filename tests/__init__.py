cf4c371c0c191598aa2029e1cfb130f826bc0686
Dan Bate <dan@wildfish.com>
2016-08-26 13:23:27 +0100

    updated ci check to use correct env var

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,7 +6,7 @@ settings.register_profile('dev', settings(max_examples=10))
 settings.register_profile('ci', settings())
 
 
-if environ.get('TRAVIC_CI', False):
+if environ.get('CI', False):
     settings.load_profile('ci')
 else:
     settings.load_profile('dev')
